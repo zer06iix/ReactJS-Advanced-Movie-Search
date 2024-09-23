18ac96bdf771c01f148792a24528aa7bcc6507a4
Sahand <mohsen.301933@gmail.com>
2024-09-24 01:32:40 +0330

    Refactor project structure & enhance UI components
    - Organized project files into distinct folders for styles, scripts, and assets to improve code maintainability and facilitate future feature updates.
    
    - Moved index.html and sprite.svg files (with updated icons) to the public folder for better file management.
    
    - Restructured JavaScript codebase for improved maintainability, separating DOM generation and event listeners into distinct files.
    
    - Replaced example prototypes with a placeholder image for future updates with actual movie posters fetched from an API.
    
    - Added a navigation bar with multiple tabs and integrated a following indicator for improved user visibility.
    
    - Segregated homepage styles into separate files for each component layout, including navigation links.
    
    - Note: Image carousel animations remain a demo and require further refinement for smoother transitions.

---
+++ src/scripts/index.js
@@ -0,0 +1,9 @@
+import {
+  navbarIndicatorAction,
+  imageCarouselAction
+} from './eventHandlers.js'
+
+document.addEventListener('DOMContentLoaded', () => {
+  navbarIndicatorAction()
+  imageCarouselAction()
+})
