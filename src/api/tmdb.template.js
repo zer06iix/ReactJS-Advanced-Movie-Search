4c5370d22868efe538abb29241f51cd64028b82c
Abolfazl Khaksar <akhaksar437@gmail.com>
2025-01-06 01:25:09 +0330

    Add tmdb.template.js file with API key instructions for collaborators

---
+++ src/api/tmdb.template.js
@@ -0,0 +1,9 @@
+// Create a new file named tmdb.js in the following directory:
+// src/api/tmdb.js
+// Please obtain the API key from the author.
+
+// Insert the following code into tmdb.js
+// const API_KEY = 'ask_author_for_api_key';
+// const BASE_URL = 'https://api.themoviedb.org/3';
+
+// export { API_KEY, BASE_URL };
