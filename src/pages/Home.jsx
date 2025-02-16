329adaa6e0520e11927963f3b6951b0e92929685
Abolfazl Khaksar <akhaksar437@gmail.com>
2025-02-16 03:59:58 +0330

    Refactor Home component to integrate show store and enhance data fetching for movies and shows (incomplete)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { useEffect, useRef } from 'react';
 import { useQuery } from '@tanstack/react-query';
 
@@ -8,31 +9,71 @@ import NavigationMenu from '../components/HomePage/NavigationMenu';
 
 import useFetchStore from '../stores/fetchStore';
 import useMovieStore from '../stores/movieStore';
+import useShowStore from '../stores/showStore';
+import useNavigationMenuStore from '../stores/navigationMenuStore';
+
 
 export default function Home() {
-    const { fetchPopularMovies } = useFetchStore();
-    const { setPopularMovies, popularMovies } = useMovieStore();
+    let navigations;
+
+    const { fetchPopularMovies, fetchTrendingMovies, fetchPopularShows, fetchTrendingShows } = useFetchStore();
+
+    const { 
+        popularMovies, setPopularMovies, 
+        trendingMovies, setTrendingMovies
+    } = useMovieStore();
+
+    const { 
+        popularShows, setPopularShows, 
+        trendingShows, setTrendingShows
+    } = useShowStore();
+
+    const { selectedIndex, setSelectedIndex } = useNavigationMenuStore();
 
     const carouselWrapperRef = useRef(null); // Ref for carousel wrapper
     const upNextWrapperRef = useRef(null); // Ref for up-next section
 
-    const page = 1;
-    const { data, error, isLoading } = useQuery({
-        queryKey: ['popularMovies', page],
-        queryFn: () => fetchPopularMovies(page)
+    const { data: popularMoviesData, error: popularMoviesError, isLoading: isPopularMoviesLoading } = useQuery({
+        queryKey: ['popularMovies'],
+        queryFn: () => fetchPopularMovies()
+    });
+
+    const { data: trendingMoviesData, error: trendingMoviesError, isLoading: isTrendingMoviesLoading } = useQuery({
+        queryKey: ['trendingMovies'],
+        queryFn: () => fetchTrendingMovies()
+    });
+
+    const { data: popularShowsData, error: popularShowsError, isLoading: isPopularShowsLoading } = useQuery({
+        queryKey: ['popularShows'],
+        queryFn: () => fetchPopularShows()
+    });
+
+    const { data: trendingShowsData, error: trendingShowsError, isLoading: isTrendingShowsLoading } = useQuery({
+        queryKey: ['trendingShows'],
+        queryFn: () => fetchTrendingShows()
     });
 
     useEffect(() => {
-        if (data && data !== popularMovies) {
-            setPopularMovies(data); // Update the store only when data changes
+        if (popularMoviesData && popularMoviesData !== popularMovies) {
+            setPopularMovies(popularMoviesData); // Update the store only when data changes
         }
-    }, [data, popularMovies, setPopularMovies]);
+        if (trendingMoviesData && trendingMoviesData !== trendingMovies) {
+            setTrendingMovies(trendingMoviesData); // Update the store only when data changes
+        }
+        if (popularShowsData && popularShowsData !== popularShows) {
+            setPopularShows(popularShowsData); // Update the store only when data changes
+        }
+        if (trendingShowsData && trendingShowsData !== trendingShows) {
+            setTrendingShows(trendingShowsData); // Update the store only when data changes
+        }
+    }, [popularMoviesData, trendingMoviesData, popularShowsData, trendingShowsData, popularMovies, trendingMovies, popularShows, trendingShows, setPopularMovies, setTrendingMovies, setPopularShows, setTrendingShows]);
 
-    if (isLoading) {
+    if (isPopularMoviesLoading || isTrendingMoviesLoading || isPopularShowsLoading || isTrendingShowsLoading) {
         return <Loading />;
     }
 
-    if (error) {
+    if (popularMoviesError || trendingMoviesError || popularShowsError || trendingShowsError) {
+        const error = popularMoviesError || trendingMoviesError || popularShowsError || trendingShowsError;
         return (
             <div className="error-container">
                 <div className="error-details-wrapper">
@@ -43,15 +84,39 @@ export default function Home() {
         );
     }
 
+    switch (selectedIndex){
+        case 0:
+            navigations = trendingMovies;
+            break;
+        case 1:
+            navigations = trendingShows;
+            break;
+        case 2:
+            navigations = popularMovies;
+            break;
+        case 3:
+            navigations = popularShows;
+            break;
+        // case 4:
+        //     navigations = newReleases;
+        //     break;
+        // case 5:
+        //     navigations = upComing;
+        //     break;
+        default:
+            navigations = [];
+            break;
+    }
+
     return (
         <>
             <NavigationMenu />
             <MediaCarousel
-                media={popularMovies}
+                media={navigations}
                 wrapperRef={carouselWrapperRef}
                 upNextWrapperRef={upNextWrapperRef}
             />
-            <UpNextSection movies={popularMovies} wrapperRef={upNextWrapperRef} />
+            <UpNextSection media={navigations} wrapperRef={upNextWrapperRef} />
         </>
     );
 }
